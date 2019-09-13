f211f47901b69a2e1307c197d58375220d2f7993
VictorGus <retaow@gmail.com>
2019-09-13 17:42:11 +0300

    swap state fix

--- src/depth_search/core.clj
+++ src/depth_search/core.clj
@@ -8,6 +8,8 @@
 (defn init-state [instate]
   (let [array-of-keys [:a :b :c :d :e :f :g :h :i]
         initialized-state (cond
+                            (= clojure.core$atom (type instate))
+                            instate
                             (vector? instate)
                             (atom (into (sorted-map) (zipmap array-of-keys instate)))
                             (list? instate)
@@ -15,11 +17,15 @@
                             (string? instate)
                             (atom (into (sorted-map) (zipmap array-of-keys (vec (map #(Character/digit % 10) (filter #(not (= % \space)) (vec instate)))))))
                             :else (throw (Exception. (str "Input value is supposed to be string, vector or list, but got " (type instate)))))]
-    (reset-meta! initialized-state {:gap (nth (keep #(if (= 0 (val %)) (key %)) @initialized-state) 0)})
+    (reset-meta! initialized-state {:gap (nth (->> @initialized-state
+                                                   (keep #(if (= 0 (val %)) (key %)))) 0)})
     initialized-state))
 
 (defn convert-state-to-string [state]
-  (string/join (vec (vals @state))))
+  (-> @state
+      vals
+      vec
+      string/join))
 
 (defn make-state-checked [state]
   (swap! checked-states #(assoc % (convert-state-to-string state) nil)))
@@ -31,5 +37,170 @@
 (defn state-goal? [state goal-state]
   (= @state @goal-state))
 
-(defn change-state [state]
-  (atom {}))
+(defn swap-state [state]
+  (let [moved-state state
+        result-state (cond
+                       (and (= :a (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :a (:b @moved-state))
+                                                                                                               (assoc :b 0)))))))
+
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :a (:b @moved-state))
+                                                    (assoc :b 0))))
+
+                       (and (= :a (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :a (:d @moved-state))
+                                                                                                               (assoc :d 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :a (:d @moved-state))
+                                                    (assoc :d 0))))
+
+                       (and (= :b (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :b (:a @moved-state))
+                                                                                                               (assoc :a 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :b (:a @moved-state))
+                                                    (assoc :a 0))))
+                       (and (= :b (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :b (:c @moved-state))
+                                                                                                               (assoc :c 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :b (:c @moved-state))
+                                                    (assoc :c 0))))
+
+                       (and (= :b (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :b (:e @moved-state))
+                                                                                                               (assoc :e 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :b (:e @moved-state))
+                                                    (assoc :e 0))))
+
+                       (and (= :c (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :c (:b @moved-state))
+                                                                                                               (assoc :b 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :c (:b @moved-state))
+                                                    (assoc :b 0))))
+
+                       (and (= :c (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :c (:f @moved-state))
+                                                                                                               (assoc :f 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :c (:f @moved-state))
+                                                    (assoc :f 0))))
+
+                       (and (= :d (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :d (:a @moved-state))
+                                                                                                               (assoc :a 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :d (:a @moved-state))
+                                                    (assoc :a 0))))
+
+                       (and (= :d (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :d (:e @moved-state))
+                                                                                                               (assoc :e 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :d (:e @moved-state))
+                                                    (assoc :e 0))))
+
+                       (and (= :d (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :d (:g @moved-state))
+                                                                                                               (assoc :g 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :d (:g @moved-state))
+                                                    (assoc :g 0))))
+
+                       (and (= :e (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :e (:f @moved-state))
+                                                                                                               (assoc :f 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :e (:f @moved-state))
+                                                    (assoc :f 0))))
+
+                       (and (= :e (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :e (:h @moved-state))
+                                                                                                               (assoc :h 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :e (:h @moved-state))
+                                                    (assoc :h 0))))
+
+                       (and (= :e (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :e (:b @moved-state))
+                                                                                                               (assoc :b 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :e (:b @moved-state))
+                                                    (assoc :b 0))))
+
+                       (and (= :e (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :e (:d @moved-state))
+                                                                                                               (assoc :d 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :e (:d @moved-state))
+                                                    (assoc :d 0))))
+                       (and (= :f (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :f (:i @moved-state))
+                                                                                                               (assoc :i 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :f (:i @moved-state))
+                                                    (assoc :i 0))))
+
+                       (and (= :g (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :g (:h @moved-state))
+                                                                                                               (assoc :h 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :g (:h @moved-state))
+                                                    (assoc :h 0))))
+
+                       (and (= :g (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :g (:d @moved-state))
+                                                                                                               (assoc :d 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :g (:d @moved-state))
+                                                    (assoc :d 0))))
+
+                       (and (= :h (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :h (:i @moved-state))
+                                                                                                               (assoc :i 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :h (:i @moved-state))
+                                                    (assoc :i 0))))
+
+                       (and (= :h (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :h (:e @moved-state))
+                                                                                                               (assoc :e 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :h (:e @moved-state))
+                                                    (assoc :e 0))))
+
+                       (and (= :h (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :h (:g @moved-state))
+                                                                                                               (assoc :g 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :h (:g @moved-state))
+                                                    (assoc :g 0))))
+
+                       (and (= :i (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :i (:h @moved-state))
+                                                                                                               (assoc :h 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :i (:h @moved-state))
+                                                    (assoc :h 0))))
+
+                       (and (= :i (:gap (meta moved-state))) (not (state-checked? (atom (into (sorted-map) (-> @moved-state
+                                                                                                               (assoc :i (:f @moved-state))
+                                                                                                               (assoc :f 0)))))))
+                       (atom (into (sorted-map) (-> @moved-state
+                                                    (assoc :i (:f @moved-state))
+                                                    (assoc :f 0)))))]
+    (make-state-checked result-state)
+    (make-state-checked state)
+    result-state))
+
+(defn search-for-goal-state [state goal-state]
+  (loop [moved-state (init-state state)]
+    (when (not (state-goal? moved-state goal-state))
+      (println @moved-state)
+      (recur (swap-state moved-state)))))
+
+(defn -main []
+  (search-for-goal-state "123405678" (init-state "123456780")))
--- test/depth_search/core_test.clj
+++ test/depth_search/core_test.clj
@@ -10,9 +10,12 @@
                                                :d 1 :e 0 :f 7
                                                :g 8 :h 2 :i 9})))
 
-(def thirdTestState (atom (into (sorted-map) {:a 3 :b 0 :c 5
-                                               :d 1 :e 4 :f 7
-                                               :g 8 :h 2 :i 9})))
+(defn add-meta-fixture [f]
+  (reset-meta! firstTestState {:gap :i})
+  (reset-meta! secondTestState {:gap :e})
+  (f))
+
+(use-fixtures :once add-meta-fixture)
 
 (deftest conversion-to-string-test
   (testing "Conversion from state to string"
@@ -35,19 +38,30 @@
                         "345170829" nil
                         "035146829" nil})
 
-(deftest change-state-test
+
+(deftest swap-state-test
   (testing "Has the state been changed properly?"
-    (is (= (change-state firstTestState) (atom (into (sorted-map) {:a 3 :b 4 :c 5
-                                                                     :d 1 :e 2 :f 0
-                                                                     :g 8 :h 9 :i 7}))))
+    (is (= @(swap-state firstTestState) @(atom (into (sorted-map) {:a 3 :b 4 :c 5
+                                                                   :d 1 :e 2 :f 0
+                                                                   :g 8 :h 9 :i 7}))))
 
-    (is (= (change-state secondTestState) (atom (into (sorted-map) {:a 3 :b 0 :c 5
-                                                                      :d 1 :e 4 :f 7
-                                                                      :g 8 :h 2 :i 9}))))
+    (is (= @(swap-state secondTestState) @(atom (into (sorted-map) {:a 3 :b 0 :c 5
+                                                                   :d 1 :e 4 :f 7
+                                                                    :g 8 :h 2 :i 9}))))))
 
-    (is (= (change-state thirdTestState) (atom (into (sorted-map) {:a 3 :b 5 :c 0
-                                                                     :d 1 :e 4 :f 7
-                                                                     :g 8 :h 2 :i 9}))))))
+;; (deftest change-state-test
+;;   (testing "Has the state been changed properly?"
+;;     (is (= (change-state firstTestState) (atom (into (sorted-map) {:a 3 :b 4 :c 5
+;;                                                                      :d 1 :e 2 :f 0
+;;                                                                      :g 8 :h 9 :i 7}))))
+
+;;     (is (= (change-state secondTestState) (atom (into (sorted-map) {:a 3 :b 0 :c 5
+;;                                                                       :d 1 :e 4 :f 7
+;;                                                                       :g 8 :h 2 :i 9}))))
+
+;;     (is (= (change-state thirdTestState) (atom (into (sorted-map) {:a 3 :b 5 :c 0
+;;                                                                      :d 1 :e 4 :f 7
+;;                                                                      :g 8 :h 2 :i 9}))))))
 
 (deftest initialize-state-test
   (testing "Has the state been initialized properly?"
